c60fa0b7cc1f44bd8de6200e868f74eaa5e0e14e
Kumar Ankur <akakankur81@gmail.com>
2020-11-18 23:10:10 +0530

    project API done

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,5 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ProjectDTO } from './project.dto';
 import { ProjectModel } from './project.model';
 import { ProjectService } from './project.service';
@@ -19,7 +32,68 @@ export class ProjectController {
     type: ProjectDTO,
   })
   async addNewProject(@Body() project: ProjectModel) {
-    const { url, title } = project;
-    console.log(url, title);
+    const { url, title, imageId } = project;
+    const newProject = await this.projectService.saveNewProject(
+      url,
+      title,
+      imageId,
+    );
+    return newProject;
+  }
+
+  @Get()
+  @ApiOkResponse({
+    description: 'Project data has been fetched successfully',
+    type: [ProjectDTO],
+  })
+  async getProject() {
+    const getProjectData = await this.projectService.getProject();
+    return getProjectData;
+  }
+
+  @Get(':id')
+  @ApiOkResponse({
+    description: 'Project has been fetched successfully',
+    type: ProjectDTO,
+  })
+  async getProjectById(@Param('id') projectId: string) {
+    const project = await this.projectService.findProject(projectId);
+    return project;
+  }
+
+  @Patch(':id')
+  @ApiCreatedResponse({
+    description: 'Project has been updated successfully',
+    type: ProjectDTO,
+  })
+  @ApiBody({
+    required: true,
+    type: ProjectDTO,
+  })
+  async updateProject(
+    @Param('id') projectId: string,
+    @Body() project: ProjectModel,
+  ) {
+    const { url, title, imageId } = project;
+    const updatedProject = await this.projectService.updateProject(
+      projectId,
+      url,
+      title,
+      imageId,
+    );
+    return updatedProject;
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({
+    description: 'Project has been deleted successfully',
+    type: ProjectDTO,
+  })
+  async deleteProject(@Param('id') projectId: string) {
+    const project = await this.projectService.deleteProject(projectId);
+    return {
+      message: 'Project has been deleted successfully',
+      project,
+    };
   }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ProjectModel } from './project.model';
@@ -8,4 +8,60 @@ export class ProjectService {
   constructor(
     @InjectModel('project') private readonly projectModel: Model<ProjectModel>,
   ) {}
+
+  async saveNewProject(url: string, title: string, imageId: string) {
+    const newProject = new this.projectModel({
+      url,
+      title,
+      imageId,
+    });
+    const project = newProject.save();
+    return project;
+  }
+
+  async getProject() {
+    const project = await this.projectModel.find();
+    return project;
+  }
+
+  async updateProject(
+    projectId: string,
+    url: string,
+    title: string,
+    imageId: string,
+  ) {
+    const project = await this.findProject(projectId);
+    if (url) {
+      project.url = url;
+    }
+    if (title) {
+      project.title = title;
+    }
+    if (imageId) {
+      project.imageId = imageId;
+    }
+    const updatedProject = await project.save();
+    return updatedProject;
+  }
+
+  async deleteProject(projectId: string) {
+    const project = await this.findProject(projectId);
+    await this.projectModel.deleteOne({
+      _id: projectId,
+    });
+    return project;
+  }
+
+  async findProject(id: string) {
+    let project;
+    try {
+      project = await this.projectModel.findById(id);
+    } catch (err) {
+      throw new NotFoundException('Error while fetching project');
+    }
+    if (!project) {
+      throw new NotFoundException('Error while fetching project');
+    }
+    return project;
+  }
 }
